62d9402d2011d25e8e3153d5c2a94188a8d69f15
Caleb Anderson <robotrising@gmail.com>
2016-02-11 22:57:55 -0700

    modify for qr

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,3 +1,4 @@
+/* modified by Caleb Anderson */
 /*!
  * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
@@ -35,6 +36,9 @@ $("a").mouseup(function(){
     $(this).blur();
 })
 
+// Centennial, CO
+var center = new google.maps.LatLng(39.5906, -104.8691);
+
 // Google Maps Scripts
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
@@ -44,17 +48,17 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+        zoom: 11,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: center,
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
         scrollwheel: false,
         draggable: false,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             "featureType": "water",
@@ -166,16 +170,21 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
 
+    // keep the map centered as the page resizes
+    google.maps.event.addDomListener(window, 'resize', function() {
+        map.setCenter(center);
+    });
+
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(39.5906, -104.8691);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
